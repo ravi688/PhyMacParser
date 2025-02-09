9c8b123f03d4b542f156a86b4da677e3931d216b
ravi688 <rp0412204@gmail.com>
2025-02-09 06:54:53 +0530

    Migrated the build system to build master

--- include/phymac_parser/defines.h
+++ include/phymac_parser/defines.h
@@ -2,13 +2,4 @@
 #pragma once
 
 #include <common/defines.h>
-
-#ifdef PPSR_STATIC_LIBRARY
-#	define PPSR_API
-#elif PPSR_DYNAMIC_LIBRARY
-#	define PPSR_API __declspec(dllimport)
-#elif BUILD_DYNAMIC_LIBRARY
-#	define PPSR_API __declspec(dllexport)
-#else
-#	define PPSR_API
-#endif
+#include <phymac_parser/api_defines.h>
